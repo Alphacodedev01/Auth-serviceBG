19e67709e6537c81a5e3430c774086eb93623562
SebastianQuesadaRios <sebastianquesadarios@gmail.com>
2025-03-20 15:15:08 -0500

    4

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  firebaseUid: {
-    type: String,
-    required: true,
-    unique: true
-  },
+  
   email: {
     type: String,
     required: true,
